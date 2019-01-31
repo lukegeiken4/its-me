ccfeabff7676f55f09fec2d3f4a55beb6bcd920f
Luke Geiken <luke.geiken@gmail.com>
2019-01-31 15:50:41 -0800

    update js maps init

--- main.js
+++ main.js
@@ -1,36 +1,22 @@
 $(document).ready(function(){
   $('.parallax').parallax();
   console.log('Setting up the maps');
-  setupTaksim();
-  setupParis();
-  setupVancouver();
 
-  google.maps.event.addDomListener(window, 'load', initialize);
+  var taksimCords = {lat: 41.037860, lng: 28.985049};
+  var taksimElId = 'taksimMap';
+  setupMap(taksimCords, taksimElId);
+
+  var parisCords = {lat: 48.862508, lng: 2.343092};
+  var parisElId = 'parisMap';
+  setupMap(parisCords, parisElId);
+
+  var vancouverCords = {lat: 49.282425, lng: -123.106959};
+  var vancouverElId = 'vancouverMap';
+  setupMap(vancouverCords, vancouverElId);
 });
 
-function setupTaksim() {
-  var taksimProp = {
-    center:new google.maps.LatLng(41.037860, 28.985049),
-    zoom:12,
-    mapTypeId:google.maps.MapTypeId.SATELLITE
-  };
-  var map=new google.maps.Map(document.getElementById("taksimMap"), taksimProp);
-}
-
-function setupParis() {
-  var taksimProp = {
-    center:new google.maps.LatLng(48.862508, 2.343092),
-    zoom:12,
-    mapTypeId:google.maps.MapTypeId.SATELLITE
-  };
-  var map=new google.maps.Map(document.getElementById("parisMap"), taksimProp);
-}
-
-function setupVancouver() {
-  var taksimProp = {
-    center:new google.maps.LatLng(49.282425, -123.106959),
-    zoom:12,
-    mapTypeId:google.maps.MapTypeId.SATELLITE
-  };
-  var map=new google.maps.Map(document.getElementById("vancouverMap"), taksimProp);
+function setupMap(cords, elementId) {
+  var map = new google.maps.Map(
+      document.getElementById(elementId), {zoom: 4, center: cords});
+  var marker = new google.maps.Marker({position: cords, map: map});
 }
