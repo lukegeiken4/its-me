e87d4b7cf3e21ba706780cbd02c121d52cb82028
Luke Geiken <luke.geiken@gmail.com>
2019-01-31 14:07:34 -0800

    failing hard

--- react-client/src/components/pages/homePage.js
+++ react-client/src/components/pages/homePage.js
@@ -11,14 +11,28 @@ class HomePage extends Component {
               <br />
               <h1 class="header center white-text">luke geiken</h1>
               <div class="row center">
-                <a class="not-active" href="https://www.linkedin.com/in/lucas-geiken-39484461" target="_blank"><i class="fa fa-linkedin-square fa-3x icon" aria-hidden="true"></i></a>
-                <a class="not-active" href="https://github.com/lukegeiken4" target="_blank"><i class="fa fa-github fa-3x icon" aria-hidden="true"></i></a>
-                <a class="not-active" href="https://twitter.com/lukieandtheg" target="_blank"><i class="fa fa-twitter-square fa-3x icon" aria-hidden="true"></i></a>
-                <a class="not-active" href="https://www.instagram.com/luke_geiken/" target="_blank"><i class="fa fa-instagram fa-3x icon" aria-hidden="true"></i></a>
-                <a class="not-active" href="mailto:luke.geiken@gmail.com?Subject=Contacting you from your website"><i class="fa fa-envelope fa-3x icon" aria-hidden="true"></i></a>
-                <a class="not-active" href="Resume.pdf" target="_blank"><i class="fa fa-file-pdf-o fa-3x icon" aria-hidden="true"></i></a>
+                <a class="not-active" href="https://www.linkedin.com/in/lucas-geiken-39484461" target="_blank">
+                  <i class="fa fa-linkedin-square fa-3x icon" aria-hidden="true" />
+                </a>
+                <a class="not-active" href="https://github.com/lukegeiken4" target="_blank">
+                  <i class="fa fa-github fa-3x icon" aria-hidden="true" />
+                </a>
+                <a class="not-active" href="https://twitter.com/lukieandtheg" target="_blank">
+                  <i class="fa fa-twitter-square fa-3x icon" aria-hidden="true" />
+                </a>
+                <a class="not-active" href="https://www.instagram.com/luke_geiken/" target="_blank">
+                  <i class="fa fa-instagram fa-3x icon" aria-hidden="true" />
+                </a>
+                <a class="not-active" href="mailto:luke.geiken@gmail.com?Subject=Contacting you from your website">
+                  <i class="fa fa-envelope fa-3x icon" aria-hidden="true" />
+                </a>
+                <a class="not-active" href="Resume.pdf" target="_blank">
+                  <i class="fa fa-file-pdf-o fa-3x icon" aria-hidden="true" />
+                </a>
               </div>
-              <a class="photo-author" href="https://unsplash.com/@paulgilmore_" target="_blank"><p class="photo-author">photo by: paul gilmore</p></a>
+              <a class="photo-author" href="https://unsplash.com/@paulgilmore_" target="_blank">
+                <p class="photo-author">photo by: paul gilmore</p>
+              </a>
             </div>
           </div>
           <div class="parallax">
@@ -32,7 +46,9 @@ class HomePage extends Component {
               <p class="center light">check out my resume to view all my work experience</p>
               <div class="col s12 m6">
                 <div class="icon-block">
-                  <h2 class="center purple-color"><i class="fa fa-windows" aria-hidden="true"></i></h2>
+                  <h2 class="center purple-color">
+                    <i class="fa fa-windows" aria-hidden="true" />
+                  </h2>
                   <h5 class="center">microsoft software engineering intern</h5> 
                   <h5 class="center">may 2016 - present</h5>
                   <div>
@@ -44,7 +60,9 @@ class HomePage extends Component {
               </div>
               <div class="col s12 m6">
                 <div class="icon-block">
-                  <h2 class="center purple-color"><i class="fa" aria-hidden="true"></i></h2>
+                  <h2 class="center purple-color">
+                    <i class="fa" aria-hidden="true" />
+                  </h2>
                   <h5 class="center">bunchball software engineering intern</h5>
                   <h5 class="center">may 2015 - may 2016</h5>
                   <div>
@@ -61,8 +79,14 @@ class HomePage extends Component {
           <div class="section no-pad-bot">
             <div class="container">
               <br />
-              <h4 class="header center white-text"><i class="fa fa-chevron-left" aria-hidden="true"></i> beautiful picture to enhance user interface <i class="fa fa-chevron-right" aria-hidden="true"></i></h4>
-              <a class="photo-author" href="https://unsplash.com/@matthewwiebe" target="_blank"><p class="photo-author">photo by: matthew wiebe</p></a>
+              <h4 class="header center white-text">
+                <i class="fa fa-chevron-left" aria-hidden="true" />
+                beautiful picture to enhance user interface 
+                <i class="fa fa-chevron-right" aria-hidden="true" />
+              </h4>
+              <a class="photo-author" href="https://unsplash.com/@matthewwiebe" target="_blank">
+                <p class="photo-author">photo by: matthew wiebe</p>
+              </a>
             </div>
           </div>
           <div class="parallax">
@@ -74,56 +98,110 @@ class HomePage extends Component {
             <div class="row">
               <div class="col s12 m6">
                 <div class="icon-block">
-                  <h2 class="center purple-color"><i class="fa fa-laptop" aria-hidden="true"></i></h2>
+                  <h2 class="center purple-color">
+                    <i class="fa fa-laptop" aria-hidden="true" />
+                  </h2>
                   <h5 class="center">technologies</h5>
                   <p>i have been able to work with many technologies in my career. some of my most noteworthy are:</p>
                   <p class="light">languages:</p>
                   <ul style="list-style-type:none">
-                    <a href="https://www.javascript.com/" target="_blank"><li>javascript</li></a>
-                    <a href="https://java.com/en/" target="_blank"><li>java</li></a>
-                    <a href="https://msdn.microsoft.com/en-us/library/kx37x362.aspx" target="_blank"><li>c#</li></a>
-                    <a href="https://www.ruby-lang.org/en/" target="_blank"><li>ruby</li></a>
+                    <a href="https://www.javascript.com/" target="_blank">
+                      <li>javascript</li>
+                    </a>
+                    <a href="https://java.com/en/" target="_blank">
+                      <li>java</li>
+                    </a>
+                    <a href="https://msdn.microsoft.com/en-us/library/kx37x362.aspx" target="_blank">
+                      <li>c#</li>
+                    </a>
+                    <a href="https://www.ruby-lang.org/en/" target="_blank">
+                      <li>ruby</li>
+                    </a>
                   </ul>
                   <p class="light">integration/ unit Testing:</p>
                   <ul style="list-style-type:none">
-                    <a href="http://www.specflow.org/" target="_blank"><li>specflow</li></a>
-                    <a href="http://jasmine.github.io/" target="_blank"><li>jasmine</li></a>
-                    <a href="http://rspec.info/" target="_blank"><li>rspec</li></a>
+                    <a href="http://www.specflow.org/" target="_blank">
+                      <li>specflow</li>
+                    </a>
+                    <a href="http://jasmine.github.io/" target="_blank">
+                      <li>jasmine</li>
+                    </a>
+                    <a href="http://rspec.info/" target="_blank">
+                      <li>rspec</li>
+                    </a>
                   </ul>
                   <p class="light">databases</p>
                   <ul style="list-style-type:none">
-                    <a href="https://azure.microsoft.com/en-us/services/documentdb/" target="_blank"><li>documentdb</li></a>
-                    <a href="https://www.elastic.co/" target="_blank"><li>elastic</li></a>
-                    <a href="https://www.mongodb.com/" target="_blank"><li>mongodb</li></a>
-                    <a href="https://www.mysql.com/" target="_blank"><li>mysql</li></a>
+                    <a href="https://azure.microsoft.com/en-us/services/documentdb/" target="_blank">
+                      <li>documentdb</li>
+                    </a>
+                    <a href="https://www.elastic.co/" target="_blank">
+                      <li>elastic</li>
+                    </a>
+                    <a href="https://www.mongodb.com/" target="_blank">
+                      <li>mongodb</li>
+                    </a>
+                    <a href="https://www.mysql.com/" target="_blank">
+                      <li>mysql</li>
+                    </a>
                   </ul>
                   <p class="light">other tech</p>
                   <ul style="list-style-type:none">
-                    <a href="https://aws.amazon.com/" target="_blank"><li>aws</li></a>
-                    <a href="https://azure.microsoft.com/en-us/" target="_blank"><li>azure</li></a>
-                    <a href="https://nodejs.org/en/" target="_blank"><li>node.js</li></a>
-                    <a href="https://azure.microsoft.com/en-us/services/service-fabric/" target="_blank"><li>service fabric</li></a>
-                    <a href="https://www.docker.com/" target="_blank"><li>docker</li></a>
-                    <a href="http://microservices.io/" target="_blank"><li>microservices architecture</li></a>
-                    <a href="https://git-scm.com/" target="_blank"><li>git</li></a>
+                    <a href="https://aws.amazon.com/" target="_blank">
+                      <li>aws</li>
+                    </a>
+                    <a href="https://azure.microsoft.com/en-us/" target="_blank">
+                      <li>azure</li>
+                    </a>
+                    <a href="https://nodejs.org/en/" target="_blank">
+                      <li>node.js</li>
+                    </a>
+                    <a href="https://azure.microsoft.com/en-us/services/service-fabric/" target="_blank">
+                      <li>service fabric</li>
+                    </a>
+                    <a href="https://www.docker.com/" target="_blank">
+                      <li>docker</li>
+                    </a>
+                    <a href="http://microservices.io/" target="_blank">
+                      <li>microservices architecture</li>
+                    </a>
+                    <a href="https://git-scm.com/" target="_blank">
+                      <li>git</li>
+                    </a>
                   </ul>
                 </div>
               </div>
               <div class="col s12 m6">
                 <div class="icon-block">
-                  <h2 class="center purple-color"><i class="fa fa-user" aria-hidden="true"></i></h2>
+                  <h2 class="center purple-color">
+                    <i class="fa fa-user" aria-hidden="true" />
+                  </h2>
                   <h5 class="center">about myself</h5>
                   <p class="light">i have spent my life up till now enjoying new experiences and new locations. i am an active traveler by feet, bike, subaru outback, and plane. enjoying new countries and continents are a thrilling experience, one that i will continue throughout the foreseeable future.</p>
                   <p class="light">along with traveling, music and art are another of my passions. writing and producing music are some of my favorite pastime activies. </p>
                   <p class="light">favorite artists: </p>
                   <ul style="list-style-type:none">
-                    <a href="https://play.spotify.com/artist/1mSJCvDX0W7Dn7S9C6vmvI?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank"><li>anna of the north</li></a>
-                    <a href="https://play.spotify.com/artist/2xe8IXgCTpwHE3eA9hTs4n?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank"><li>banks</li></a>
-                    <a href="https://play.spotify.com/artist/3FMbhaYng6VoeCltbwqmtG?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank"><li>brika</li></a>
-                    <a href="https://play.spotify.com/artist/2Q0MyH5YMI5HPQjFjlq5g3?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank"><li>chet faker</li></a>
-                    <a href="https://play.spotify.com/artist/5cIc3SBFuBLVxJz58W2tU9?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank"><li>oh wonder</li></a>
-                    <a href="https://play.spotify.com/artist/1TtJ8j22Roc24e2Jx3OcU4?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank"><li>purity ring</li></a>
-                    <a href="https://play.spotify.com/artist/100sLnojEpcadRx4edEBA6?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank"><li>zella day</li></a>
+                    <a href="https://play.spotify.com/artist/1mSJCvDX0W7Dn7S9C6vmvI?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank">
+                      <li>anna of the north</li>
+                    </a>
+                    <a href="https://play.spotify.com/artist/2xe8IXgCTpwHE3eA9hTs4n?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank">
+                      <li>banks</li>
+                    </a>
+                    <a href="https://play.spotify.com/artist/3FMbhaYng6VoeCltbwqmtG?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank">
+                      <li>brika</li>
+                    </a>
+                    <a href="https://play.spotify.com/artist/2Q0MyH5YMI5HPQjFjlq5g3?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank">
+                      <li>chet faker</li>
+                    </a>
+                    <a href="https://play.spotify.com/artist/5cIc3SBFuBLVxJz58W2tU9?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank">
+                      <li>oh wonder</li>
+                    </a>
+                    <a href="https://play.spotify.com/artist/1TtJ8j22Roc24e2Jx3OcU4?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank">
+                      <li>purity ring</li>
+                    </a>
+                    <a href="https://play.spotify.com/artist/100sLnojEpcadRx4edEBA6?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank">
+                      <li>zella day</li>
+                    </a>
                   </ul>
                   <p class="light">my art includes drawing and tattooing. i am currently an amature tattooist using traditional forms of tattooing to put down my art. you are able to check our some of my work on my instagram page linked above, or:</p>
                   <a href="https://www.instagram.com/p/BFNtokQs0OP/?taken-by=luke_geiken" target="_blank">check out my favorite tattoo piece i've done</a>
@@ -139,8 +217,14 @@ class HomePage extends Component {
           <div class="section no-pad-bot">
             <div class="container">
               <br />
-              <h4 class="header center white-text"><i class="fa fa-chevron-left" aria-hidden="true"></i> im currently living here. its pretty cool <i class="fa fa-chevron-right" aria-hidden="true"></i></h4>
-              <a class="photo-author" href="https://unsplash.com/@jswift" target="_blank"><p class="photo-author">photo by: josh swift</p></a>
+              <h4 class="header center white-text">
+                <i class="fa fa-chevron-left" aria-hidden="true" />
+                im currently living here. its pretty cool 
+                <i class="fa fa-chevron-right" aria-hidden="true" />
+              </h4>
+              <a class="photo-author" href="https://unsplash.com/@jswift" target="_blank">
+                <p class="photo-author">photo by: josh swift</p>
+              </a>
             </div>
           </div>
           <div class="parallax">
@@ -153,7 +237,9 @@ class HomePage extends Component {
               <h5 class="header center purple-color">favorite locations in the world</h5>
               <div class="col s12 m4">
                 <div class="icon-block">
-                  <h2 class="center purple-color"><i class="fa fa-map-marker" aria-hidden="true"></i></i></h2>
+                  <h2 class="center purple-color">
+                    <i class="fa fa-map-marker" aria-hidden="true" />
+                  </h2>
                   <h5 class="center">taksim</h5> 
                   <h5 class="center">istanbul, turkey</h5>
                   <div id="taksimMap" style="width:100%; height:300px;"></div>
@@ -161,7 +247,9 @@ class HomePage extends Component {
               </div>
               <div class="col s12 m4">
                 <div class="icon-block">
-                  <h2 class="center purple-color"><i class="fa fa-map-marker" aria-hidden="true"></i></i></h2>
+                  <h2 class="center purple-color">
+                    <i class="fa fa-map-marker" aria-hidden="true" />
+                  </h2>
                   <h5 class="center">all districts</h5>
                   <h5 class="center">paris, france</h5>
                   <div id="parisMap" style="width:100%; height:300px;">
@@ -181,8 +269,8 @@ class HomePage extends Component {
               </div>
             </div>
           </div>
-  </div>
-  </body>
+        </div>
+      </body>
     )
   }
 }
