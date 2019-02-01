ab41deee64a3e104ccdf1c64d677204549a13ef0
Luke Geiken <luke.geiken@gmail.com>
2019-01-31 16:12:22 -0800

    fuck me, nvm. not a syntax issue. it lies elsewhere. moving back to old

--- main.js
+++ main.js
@@ -7,18 +7,18 @@ function initMaps() {
 
   var taksimCords = new google.maps.LatLng(41.037860, 28.985049);
   var taksimElId = 'taksimMap';
-  initMaps(taksimCords, taksimElId);
+  setupMap(taksimCords, taksimElId);
 
   var parisCords = new google.maps.LatLng(48.862508, 2.343092);
   var parisElId = 'parisMap';
-  initMaps(parisCords, parisElId);
+  setupMap(parisCords, parisElId);
 
   var vancouverCords = new google.maps.LatLng(49.282425, -123.106959);
   var vancouverElId = 'vancouverMap';
-  initMaps(vancouverCords, vancouverElId);
+  setupMap(vancouverCords, vancouverElId);
 }
 
-function initMaps(cords, elementId) {
+function setupMap(cords, elementId) {
   var map = new google.maps.Map(
       document.getElementById(elementId), {zoom: 4, center: cords});
   var marker = new google.maps.Marker({position: cords, map: map});
