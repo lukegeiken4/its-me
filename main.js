164f689a4dc7e2b66308c9f3f2d9ff04218b7f51
Luke Geiken <luke.geiken@gmail.com>
2019-01-31 16:21:57 -0800

    using double quotes to see what happens

--- main.js
+++ main.js
@@ -6,20 +6,20 @@ function initMaps() {
   console.log('Setting up the maps');
 
   var taksimCords = new google.maps.LatLng(41.037860, 28.985049);
-  var taksimElId = 'taksimMap';
+  var taksimElId = "taksimMap";
   console.log(document);
   var taksimEl = document.getElementById(taksimElId);
   console.log(taksimEl);
   setupMap(taksimCords, taksimEl);
 
   var parisCords = new google.maps.LatLng(48.862508, 2.343092);
-  var parisElId = 'parisMap';
+  var parisElId = "parisMap";
   var parisEl = document.getElementById(parisElId);
   console.log(parisEl);
   setupMap(parisCords, parisEl);
 
   var vancouverCords = new google.maps.LatLng(49.282425, -123.106959);
-  var vancouverElId = 'vancouverMap';
+  var vancouverElId = "vancouverMap";
   var vancouverEl = document.getElementById(vancouverElId);
   console.log(vancouverEl);
   setupMap(vancouverCords, vancouverEl);
