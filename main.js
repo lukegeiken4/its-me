9bfd6544724d5dfee9fcdc5e81e893539c9a13a6
Luke Geiken <luke.geiken@gmail.com>
2019-01-31 16:11:30 -0800

    syntax issue

--- main.js
+++ main.js
@@ -7,18 +7,18 @@ function initMaps() {
 
   var taksimCords = new google.maps.LatLng(41.037860, 28.985049);
   var taksimElId = 'taksimMap';
-  setupMap(taksimCords, taksimElId);
+  initMaps(taksimCords, taksimElId);
 
   var parisCords = new google.maps.LatLng(48.862508, 2.343092);
   var parisElId = 'parisMap';
-  setupMap(parisCords, parisElId);
+  initMaps(parisCords, parisElId);
 
   var vancouverCords = new google.maps.LatLng(49.282425, -123.106959);
   var vancouverElId = 'vancouverMap';
-  setupMap(vancouverCords, vancouverElId);
+  initMaps(vancouverCords, vancouverElId);
 }
 
-function setupMap(cords, elementId) {
+function initMaps(cords, elementId) {
   var map = new google.maps.Map(
       document.getElementById(elementId), {zoom: 4, center: cords});
   var marker = new google.maps.Marker({position: cords, map: map});
