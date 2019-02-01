55d1c35a7c078a7b624b8c0ff0fc2df27aee3445
Luke Geiken <luke.geiken@gmail.com>
2019-01-31 16:06:57 -0800

    using google maps latlong model

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
 
 function initMaps() {
   console.log('Setting up the maps');
-  
-  var taksimCords = {lat: 41.037860, lng: 28.985049};
+
+  var taksimCords = new google.maps.LatLng(41.037860, 28.985049);
   var taksimElId = 'taksimMap';
   setupMap(taksimCords, taksimElId);
 
-  var parisCords = {lat: 48.862508, lng: 2.343092};
+  var parisCords = new google.maps.LatLng(48.862508, 2.343092);
   var parisElId = 'parisMap';
   setupMap(parisCords, parisElId);
 
-  var vancouverCords = {lat: 49.282425, lng: -123.106959};
+  var vancouverCords = new google.maps.LatLng(49.282425, -123.106959);
   var vancouverElId = 'vancouverMap';
   setupMap(vancouverCords, vancouverElId);
 }
