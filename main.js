5c4c391554016265a920cc53c52aed3294919d51
Luke Geiken <luke.geiken@gmail.com>
2019-01-31 16:43:12 -0800

    add new google maps

--- main.js
+++ main.js
@@ -10,17 +10,17 @@ $(document).ready(function(){
   console.log(taksimEl);
   setupMap(taksimCords, taksimEl);
 
-  var parisCords = new google.maps.LatLng(48.862508, 2.343092);
-  var parisElId = "parisMap";
-  var parisEl = document.getElementById(parisElId);
-  console.log(parisEl);
-  setupMap(parisCords, parisEl);
+  var bledCords = new google.maps.LatLng(46.364355, 13.093704);
+  var bledElId = "bledMap";
+  var bledEl = document.getElementById(bledElId);
+  console.log(bledEl);
+  setupMap(bledCords, bledEl);
 
-  var vancouverCords = new google.maps.LatLng(49.282425, -123.106959);
-  var vancouverElId = "vancouverMap";
-  var vancouverEl = document.getElementById(vancouverElId);
-  console.log(vancouverEl);
-  setupMap(vancouverCords, vancouverEl);
+  var berlinCords = new google.maps.LatLng(52.475413, 13.401801);
+  var berlinElId = "berlinMap";
+  var berlinEl = document.getElementById(berlinElId);
+  console.log(berlinEl);
+  setupMap(berlinCords, berlinEl);
 });
 
 function setupMap(cords, el) {
