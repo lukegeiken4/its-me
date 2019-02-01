37835adb562e8cb6ecdc4aa7acfa1f5cc4560dbd
Luke Geiken <luke.geiken@gmail.com>
2019-01-31 16:16:25 -0800

    testing out why el isnt being ofund

--- main.js
+++ main.js
@@ -7,19 +7,24 @@ function initMaps() {
 
   var taksimCords = new google.maps.LatLng(41.037860, 28.985049);
   var taksimElId = 'taksimMap';
-  setupMap(taksimCords, taksimElId);
+  var taksimEl = document.getElementById(taksimElId);
+  console.log(taksimEl);
+  setupMap(taksimCords, taksimEl);
 
   var parisCords = new google.maps.LatLng(48.862508, 2.343092);
   var parisElId = 'parisMap';
-  setupMap(parisCords, parisElId);
+  var parisEl = document.getElementById(parisElId);
+  console.log(parisEl);
+  setupMap(parisCords, parisEl);
 
   var vancouverCords = new google.maps.LatLng(49.282425, -123.106959);
   var vancouverElId = 'vancouverMap';
-  setupMap(vancouverCords, vancouverElId);
+  var vancouverEl = document.getElementById(vancouverElId);
+  console.log(vancouverEl);
+  setupMap(vancouverCords, vancouverEl);
 }
 
-function setupMap(cords, elementId) {
-  var map = new google.maps.Map(
-      document.getElementById(elementId), {zoom: 4, center: cords});
+function setupMap(cords, el) {
+  var map = new google.maps.Map(el, {zoom: 4, center: cords});
   var marker = new google.maps.Marker({position: cords, map: map});
 }
